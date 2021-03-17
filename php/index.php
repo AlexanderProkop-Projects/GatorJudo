8ff1bd8796610009f07d990a02dde3700817fdd5
LRW <LordRichardWellington@live.com>
2021-03-16 20:05:45 -0400

    Checking that my php works with apache properly.

--- php/index.php
+++ php/index.php
@@ -5,9 +5,12 @@
 //This is where the session and cookies will be checked/saved/loaded.
 
 require('../html/head.php'); //This should contain all the header information.
-require('../html/body.php'); //This should contain all the stuff for the content of the website.
-require('../html/foot.php'); //This should contain all the stuff for the footer.
+//require('../html/body.php'); //This should contain all the stuff for the content of the website.
+//require('../html/foot.php'); //This should contain all the stuff for the footer.
 
 // require('./PHP-MySQL-CLOSE.php'); //This should close any open database connection (if needed).
 
 ?>
+
+</body>
+</html>
