c460720f3933bff4dff3c02af0ee2facf29df47d
Ethan <ethan@localhost.localdomain>
2021-03-23 15:14:57 -0400

    trying out with the Laravel testing thing

--- html/head.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<meta charset="utf-8" />
-		<title>Gator Judo</title>
-		<link rel="stylesheet" href="./CSS/index.css"/>
-	</head>
-<body>
--- php/UnitTests/TestTest.php
+++
@@ -1,4 +0,0 @@
-<?php 
-if(1==2)
-    exit("please fail CircleCI");
-?> 
