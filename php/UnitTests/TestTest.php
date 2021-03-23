49325c7197b782c9961a543aace0478c35c2bb90
Ethan <ethan@localhost.localdomain>
2021-03-23 14:02:21 -0400

    Try 15 testing some code I found on the internet (should pass)

--- php/UnitTests/TestTest.php
+++ php/UnitTests/TestTest.php
@@ -1,6 +1,19 @@
-<?php
+<?php 
 use PHPUnit\Framework\TestCase; 
-
-assertTrue(True);
-
-?>
+    
+class GeeksPhpunitTestCase extends TestCase 
+{ 
+    public function testPositiveForassertTrue()
+          
+    {  
+     $assertvalue = true;
+        // Assert function to test whether assert
+        // value is true or not
+        $this->assertTrue(
+            $assertvalue,
+            "assert value is true or not"
+        );
+          
+    }
+ } 
+?> 
