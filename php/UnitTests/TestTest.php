ce0e1abff9e002912bf3fdfccdd00ff052a03b98
Ethan <ethan@localhost.localdomain>
2021-03-23 14:43:05 -0400

    Try 16 should fail

--- php/UnitTests/TestTest.php
+++ php/UnitTests/TestTest.php
@@ -1,19 +1,7 @@
 <?php 
-use PHPUnit\Framework\TestCase; 
-    
-class GeeksPhpunitTestCase extends TestCase 
-{ 
-    public function testPositiveForassertTrue()
-          
-    {  
-     $assertvalue = true;
-        // Assert function to test whether assert
-        // value is true or not
-        $this->assertTrue(
-            $assertvalue,
-            "assert value is true or not"
-        );
-          
-    }
- } 
+if(True)
+{
+    die("should not run");
+}
+assert(1==1);
 ?> 
