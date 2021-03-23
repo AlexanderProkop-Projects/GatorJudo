f28559351709feee785605a4d7cdc540c750bc19
Ethan <ethan@localhost.localdomain>
2021-03-23 13:45:27 -0400

    Trying again (try 11 I think) Should Pass

--- php/UnitTests/TestTest.php
+++ php/UnitTests/TestTest.php
@@ -1,22 +1,5 @@
 <php?
-public function testBasicTest()
-{
-    $this->assertTrue(False);
-}
 
-public function testUserCreation()
-{
-    $user = new User([
-        'name' => "Test User",
-        'email' => "test@mail.com",
-        'password' => bcrypt("testpassword")
-    ]);   
-
-    $this->assertEquals('Test User', $user->name);
-}
-
-
-testBasicTest();
-testUserCreation();
+assertTrue(True);
 
 ?>
