5833f2b4555f73528e1cd002b7d757469edafdaf
LRW <LordRichardWellington@live.com>
2021-03-25 15:07:04 -0400

    Demo

--- php/SQL_Functions.php
+++ php/SQL_Functions.php
@@ -1,27 +1,56 @@
 <?php
 
+	//Independent connect function.
+	function connect(){
+		$servername = "localhost";
+		$username = "root";
+		$password = "root";
+		$dbname = "gatorjudo";
+		return new mysqli($servername, $username, $password, $dbname);
+	}
+	
+	//Function to remove writing the same if statement all the time.
+	//Passes back a result, but you don't strictly need to catch it.
+	function qy($sql){
+		$conn = connect();
+		$result = $conn->query($sql);
+		if($result === FALSE){
+			echo "ERROR: " . $conn->error . "<br>";
+		}
+		$conn->close();
+		return $result;
+	}
+	
+	//The following functions create SQL commands and send them to the query function.
 	//insert
-	function insertEntry($conn, $table, $insert){
+	function insertEntry($table, $insert){
+		
 		$parameters = "";
 		if($table === "Tournament"){
 			$parameters = "(ID)";
+			//echo "Tournament";
 		}
 		else if($table === "Announcement"){
-			$parameters = "(ID, uID, info)"; 
+			$parameters = "(uID, title, info)";
+			//echo "Announcement";
+		}
+		else if($table == "Users"){
+			$parameters = "(name, email, password)";
 		}
 		
 		$staticInsert = "INSERT INTO " . $table . " " . $parameters . " VALUES (" . $insert . ");";
-		
-		if($conn->query($staticInsert) === True){
+		qy($staticInsert);
+		/*if($conn->query($staticInsert) === True){
 			$last_id = $conn->insert_id;
 			echo "NEW RECORD CREATED SUCCESSFULLY, LAST ID ENTERED IS: " . $last_id . "<br>";
 		}
 		else{
 			echo "ERROR: " . $staticInsert . " " . $conn->error;
-		}
+		}*/
 	}
 	
-	function deleteEntry($conn, $table, $insert){
+	//Delete
+	function deleteEntry($table, $insert){
 		$parameters = "";
 		if($table === "Tournament"){
 			$parameters = "ID";
@@ -30,12 +59,29 @@
 			$parameters = "uID"; 
 		}
 		$staticDelete = "DELETE FROM " . $table . " WHERE ". $parameters . "=" . $insert . ";";
-		if($conn->query($staticDelete) === True){
+		qy($staticDelete);
+		
+		/*if($conn->query($staticDelete) === True){
 			$last_id = $conn->insert_id;
 			echo "RECORD DELETED SUCCESSFULLY, LAST ID ENTERED IS: " . $last_id . "<br>";
 		}
 		else{
 			echo "ERROR: " . $staticDelete . " " . $conn->error;
+		}*/
+	}
+	
+	function displayAnnounce(){ //This function goes right in line where a table would be.
+	//This function will probably be greatly overhauled once more work has gone into the html/css.
+		//echo "This is an announce echo.";
+		$sql = "SELECT users.name, Announcement.title, Announcement.info FROM Users, Announcement where users.ID = Announcement.uID";
+		$result = qy($sql);
+		while($row = $result->fetch_assoc()){
+			echo "<tr>";
+			echo "<td>" . $row['name'] . "</td>";
+			echo "<td>" . $row['title'] . "</td>";
+			echo "<td>" . $row['info'] . "</td>";
+			echo "</tr>";
 		}
 	}
+
 ?>
--- php/index.php
+++ php/index.php
@@ -1,19 +1,22 @@
 <?php
 
-// require('./PHP-FUNCTIONS.php');
-// require('./PHP-MySQL-Connect.php');
 //This is where the session and cookies will be checked/saved/loaded.
+
+
+//include 'SQL_Structure.php';
+include 'SQL_Functions.php';
+//insertEntry("Users", "\"Jeff\", \"FAKE_EMAIL@AOL.COM\", \"password\"");
+//insertEntry("Users", "\"Dale\", \"ITSDALE@AOL.COM\", \"password\"");
+//insertEntry("Users", "\"Chad\", \"Total_chad@AOL.COM\", \"Chad_dad\"");
+insertEntry("Announcement", "1,\"THIS IS A TITLE\" , \"This is an announcement\"");
+insertEntry("Announcement", "3,\"CHAD WAS HERE\" , \"THIS ANNOUNCEMENT BROUGHT TO YOU BY THE AWESOME CHAD\"");
 
 require('../html/head.html'); //This should contain all the header information.
 require('../html/index.html');
-include 'SQL_Structure.php';
-include 'SQL_Functions.php';
-insertEntry($conn, "Announcement", "10, 1, \"This is an announcement\"");
-deleteEntry($conn, "Announcement", "1");
-$conn->close();
 //require('../html/body.php'); //This should contain all the stuff for the content of the website.
 //require('../html/foot.php'); //This should contain all the stuff for the footer.
-
+deleteEntry("Announcement", "1");
+deleteEntry("Announcement", "3");
 // require('./PHP-MySQL-CLOSE.php'); //This should close any open database connection (if needed).
 
 ?>
