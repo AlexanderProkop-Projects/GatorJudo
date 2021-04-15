5e9492bf0c9ee15bac0c3fc31b3346553343f957
Ethan <ethan@localhost.localdomain>
2021-04-15 14:41:48 -0400

    creating a new branch with facebook things for me and Alex to work on

---
+++ GJudo-App/php/index.php
@@ -0,0 +1,23 @@
+<?php
+
+//This is where the session and cookies will be checked/saved/loaded.
+
+//echo "Hello World";
+//include 'SQL_Structure.php';
+include 'SQL_Functions.php';
+//insertEntry("Users", "\"Jeff\", \"FAKE_EMAIL@AOL.COM\", \"password\"");
+//insertEntry("Users", "\"Dale\", \"ITSDALE@AOL.COM\", \"password\"");
+//insertEntry("Users", "\"Chad\", \"Total_chad@AOL.COM\", \"Chad_dad\"");
+//insertEntry("Announcement", "1,\"THIS IS A TITLE\" , \"This is an announcement\"");
+//insertEntry("Announcement", "3,\"CHAD WAS HERE\" , \"THIS ANNOUNCEMENT BROUGHT TO YOU BY THE AWESOME CHAD\"");
+
+require('../html/head.html'); //This should contain all the header information.
+require('../html/index.html');
+deleteEntry("Announcement", "1");
+deleteEntry("Announcement", "3");
+//deleteEntry("Users", "'Jeff'");
+//deleteEntry("Users", "'Dale'");
+//deleteEntry("Users", "'Chad'");
+//require('./PHP-MySQL-CLOSE.php'); //This should close any open database connection (if needed).
+
+?>
