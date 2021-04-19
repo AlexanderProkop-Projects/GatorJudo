748933ac7576672c7f644ee97e7fe3a2cde2418c
AlexanderProkop-Projects <77085128+AlexanderProkop-Projects@users.noreply.github.com>
2021-04-18 20:35:52 -0400

    Add files via upload

---
+++ Facebook.php
@@ -0,0 +1,75 @@
+<?php
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+
+require_once './vendor/facebook/graph-sdk/src/Facebook/autoload.php';
+require_once './vendor/facebook/graph-sdk/src/Facebook/Exceptions/FacebookResponseException.php';
+require_once './vendor/facebook/graph-sdk/src/Facebook/Exceptions/FacebookSDKException.php';
+require_once './vendor/facebook/graph-sdk/src/Facebook/Helpers/FacebookRedirectLoginHelper.php';
+
+
+$fb = new \Facebook\Facebook([
+  'app_id' => '501889450839518',           //Replace {your-app-id} with your app ID
+  'app_secret' => '80dc6bbd7a4b1b24fdc4ed35b2f1fd12',   //Replace {your-app-secret} with your app secret
+  'default_graph_version' => 'v10.0'
+
+]);
+
+$accessToken = 'EAAHIdz5vDd4BAP3yaO2i9SzRHXWrGB1F0qs3af3qbTAuQ6SjwJvqZCj1gOZAe1vlG4xJr3XMqs5ZAXvCetSOYrbFqVPnDATDKFGU8YzNLaO4SYiXHqWW0WzfmEUZBkg7Nnx3N2Rx3phtE2gdsDQ9NdSjhNovZCCNJ2Y1v0iXq5gZDZD';
+//*/
+
+$postData = "";
+
+try {
+$gjudo_feed = $fb->get('/1185506478585981/feed', $accessToken);
+$postBody = $gjudo_feed->getDecodedBody();
+$postData = $postBody["data"];
+} catch (FacebookResponseException $e) {
+    echo 'ERROR 1';
+    exit();
+} catch (FacebookSDKException $e) {
+    echo 'ERROR2';
+    exit();
+}
+
+
+if (empty($postData))
+{
+    echo "empty";
+}
+
+date_default_timezone_set('America/New_York');
+
+if (! empty($postData)) {
+    foreach ($postData as $k => $v) {
+        $postDate = date("Y-m-d H:i:s", strtotime($postData[$k]["updated_time"]));
+        $keys = array_keys($postData[$k]);
+        
+        /*if("message" == $keys[1]){
+            echo "The stirngs are equal, WTF?"; //This prints.
+        }*/
+        $message = $postData[$k][$keys[1]];
+        
+        //echo "Message: " . $message . ", Date: " . $postDate;
+        
+
+        //$test1 = "test";
+        //$test2 = "2021-04-16 22:24:07";
+        
+        //$message = substr($k[]$v["message"],0,15);
+        //echo $message . "<br/>";
+        $ID = insertAnnouncement($message, $postDate, 1);
+        if (substr($message,0,15) == "Practice Update") { //Passes and echoes
+            //echo "<br/>PRACTICE<br/>";
+            insertTag($ID, "PRACTICE", 1);
+        }
+        
+        else if (substr($message,0,15) == "Tournament Info") { //Passes and echoes
+            //echo "<br/>PRACTICE<br/>";
+            insertTag($ID, "TOURNAMENT", 1);
+        }
+    }
+}
+
+?>
---
+++ SQL_Functions.php
@@ -0,0 +1,166 @@
+<?php
+    //test
+	//Independent connect function.
+	function connect(){
+		$servername = "localhost";
+		$username = "root";
+		$password = "root";
+		$dbname = "gatorjudo";
+		return new mysqli($servername, $username, $password, $dbname);
+	}
+	
+	//Function to remove writing the same if statement all the time.
+	//Passes back a result, but you don't strictly need to catch it.
+	function qy($sql){
+		$conn = connect();
+		$result = $conn->query($sql);
+		if($result === FALSE){
+			echo "ERROR: " . $conn->error . "<br>";
+		}
+		$conn->close();
+		return $result;
+	}
+	
+	//The following functions create SQL commands and send them to the query function.
+	//insert
+	function insertEntry($table, $insert){
+		
+		$parameters = "";
+		if($table === "Tournament"){
+			$parameters = "(ID)";
+			//echo "Tournament";
+		}
+		else if($table === "Announcement"){
+			$parameters = "(uID, title, info)";
+			//echo "Announcement";
+		}
+		else if($table == "Users"){
+			$parameters = "(name, email, password)";
+		}
+		
+		$staticInsert = "INSERT INTO " . $table . " " . $parameters . " VALUES (" . $insert . ");";
+		qy($staticInsert);
+		/*if($conn->query($staticInsert) === True){
+			$last_id = $conn->insert_id;
+			echo "NEW RECORD CREATED SUCCESSFULLY, LAST ID ENTERED IS: " . $last_id . "<br>";
+		}
+		else{
+			echo "ERROR: " . $staticInsert . " " . $conn->error;
+		}*/
+	}
+	
+	function insertAnnouncement($message, $time, $uID){
+        $exist = "SELECT * FROM Announcement WHERE time = '" . $time . "' AND info = \"" . $message . "\"";
+        $result = qy($exist);
+        
+        if (!$result->fetch_assoc()) {
+            $Insert = "INSERT INTO " . "Announcement " . "(info, time, uID)" . " VALUES (\"" . $message . "\", \"" . $time . "\", \"" . $uID . "\");";
+            qy($Insert);
+        }
+        
+        $findID = "SELECT ID FROM Announcement WHERE time = '" . $time . "' AND info = \"" . $message . "\"";
+        $result2 = qy($findID)->fetch_assoc();
+        $ID = $result2['ID'];
+        return $ID;
+	}
+	
+	function insertUser($name, $email, $password) {
+        $exist = "SELECT * FROM Users WHERE name = \"" . $name . "\" AND email = \"" . $email . "\" AND password = \"" . $password . "\"";
+        $result = qy($exist);
+        
+        if (!$result->fetch_assoc()) {
+            $Insert = "INSERT INTO " . "Users " . "(name, email, password)" . " VALUES (\"" . $name . "\", \"" . $email . "\", \"" . $password . "\");";
+            qy($Insert);
+        }
+	}
+	
+	function defineTag($name, $descriptor) {
+        $exist = "SELECT * FROM Tags WHERE name = \"" . $name . "\"";
+        $result = qy($exist);
+        
+        if (!$result->fetch_assoc()) {
+            $Insert = "INSERT INTO " . "Tags " . "(name, descriptor)" . " VALUES (\"" . $name . "\", \"" . $descriptor . "\");";
+            qy($Insert);
+        }
+	}
+	
+	function insertTag($aID, $tagName) {
+        $exist = "SELECT * FROM Define WHERE aID = \"" . $aID . "\" AND tag = \"" . $tagName . "\"";
+        $result = qy($exist);
+    
+        if (!$result->fetch_assoc()) {
+            $Insert = "INSERT INTO " . "Define " . "(aID, tag)" . " VALUES (\"" . $aID . "\", \"" . $tagName . "\");";
+            qy($Insert);
+        }
+	}
+	
+	
+	//Delete
+	function deleteEntry($table, $insert){
+		$parameters = "";
+		if($table === "Tournament"){
+			$parameters = "ID";
+		}
+		else if($table === "Announcement"){
+			$parameters = "uID"; 
+		}
+		else if($table === "Users"){
+            $parameters = "name";
+		}
+		$staticDelete = "DELETE FROM " . $table . " WHERE ". $parameters . "=" . $insert . ";";
+		qy($staticDelete);
+		
+		/*if($conn->query($staticDelete) === True){
+			$last_id = $conn->insert_id;
+			echo "RECORD DELETED SUCCESSFULLY, LAST ID ENTERED IS: " . $last_id . "<br>";
+		}
+		else{
+			echo "ERROR: " . $staticDelete . " " . $conn->error;
+		}*/
+	}
+	
+	function getMaxID(){
+        $sql = "SELECT Max(ID) AS MAX FROM Announcement";
+        $result = qy($sql)->fetch_assoc();
+        return $result['MAX'];	
+	}
+	
+	function getAnnouncement($ID){ //This function goes right in line where a table would be.
+	//This function will probably be greatly overhauled once more work has gone into the html/css.
+		//echo "This is an announce echo.";
+		$sql = "SELECT Users.name, Announcement.info, Announcement.time FROM Users, Announcement WHERE Users.ID = Announcement.uID AND Announcement.ID = " . $ID;
+		$result = qy($sql)->fetch_assoc();
+		
+		return array($result['name'], $result['info'], $result['time']);
+		/*while($row = $result->fetch_assoc()){
+			echo "<tr>";
+			echo "<td>" . $row['name'] . "</td>";
+			echo "<td>" . $row['title'] . "</td>";
+			echo "<td>" . $row['info'] . "</td>";
+			echo "</tr>";
+		}*/
+	}
+	
+    function getPracticeAnnouncement($ID){
+
+		$sql = "SELECT name, Announcement.info, Announcement.time
+                FROM Announcement JOIN Users JOIN Define
+                WHERE Announcement.uID = Users.ID AND Announcement.ID = Define.aID AND tag = \"Practice\" AND Announcement.ID = " . $ID;
+                
+		$result = qy($sql)->fetch_assoc();
+		
+		return array($result['name'], $result['info'], $result['time']);
+	}
+	
+    function getTournamentAnnouncement($ID){
+
+		$sql = "SELECT name, Announcement.info, Announcement.time
+                FROM Announcement JOIN Users JOIN Define
+                WHERE Announcement.uID = Users.ID AND Announcement.ID = Define.aID AND tag = \"TOURNAMENT\" AND Announcement.ID = " . $ID;
+                
+		$result = qy($sql)->fetch_assoc();
+		
+		return array($result['name'], $result['info'], $result['time']);
+	}
+
+?>
