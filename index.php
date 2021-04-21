c39d70530a62839c0930d5fe974039edfb38fcc2
AlexanderProkop-Projects <77085128+AlexanderProkop-Projects@users.noreply.github.com>
2021-04-21 16:59:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
     //include 'SQL_Structure.php
-	include_once('index.html');
-    include 'SQL_Functions.php';
+	//include_once('index.html');
+    //include 'SQL_Functions.php';
     insertUser("Facebook", "Facebook.com", "no password");
     defineTag("PRACTICE", "A practice announcement");
     defineTag("TOURNAMENT", "A tournament announcement");
